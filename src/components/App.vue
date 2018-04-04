06c118515bdfef808f12f2f8609aa1dd7bc3b9b6
nitigan <nitigan.n@fitm.kmutnb.ac.th>
2018-04-04 16:41:35 +0700

    HW Movie

---
+++ src/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box">
+    <h3 class="title">App: {{ movieId }}</h3>
+    <movie @chooseMovie="handleChooseMovie" :movieId="movieId" />
+  </div>
+</template>
+
+<script>
+  import Movie from 'Components/Movie.vue'
+
+  export default{
+    components: { Movie },
+    data(){
+      return{
+          movieId: ''
+      }
+    },
+    methods: {
+      handlechooseMovie(movieId){
+        this.movieId = movieId
+      }
+    }
+  }
+</script>
